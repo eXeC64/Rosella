b245c56143675d2385ac8b9508ed0de89dd39a35
Harry Jeffery <harry@exec64.co.uk>
2013-08-30 01:29:02 +0100

    Changed order of TLS ciphers

--- main.go
+++ main.go
@@ -58,14 +58,14 @@ func main() {
 
 	tlsConfig.PreferServerCipherSuites = true
 	tlsConfig.CipherSuites = []uint16{
-		tls.TLS_ECDHE_RSA_WITH_RC4_128_SHA,
-		tls.TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA,
-		tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
 		tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
-		tls.TLS_RSA_WITH_RC4_128_SHA,
-		tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA,
+		tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+		tls.TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA,
+		tls.TLS_ECDHE_RSA_WITH_RC4_128_SHA,
+		tls.TLS_RSA_WITH_AES_256_CBC_SHA,
 		tls.TLS_RSA_WITH_AES_128_CBC_SHA,
-		tls.TLS_RSA_WITH_AES_256_CBC_SHA}
+		tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA,
+		tls.TLS_RSA_WITH_RC4_128_SHA}
 
 	cert, err := tls.LoadX509KeyPair(*tlsCertFile, *tlsKeyFile)
 	if err != nil {
